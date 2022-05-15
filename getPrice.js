ce867ae847b6eaefd6d0e968d5c32a27b7887ff1
Théo Cramez <theo.cramez@gmail.com>
2022-05-15 20:05:03 +0200

    add url check

--- getPrice.js
+++ getPrice.js
@@ -8,8 +8,17 @@ const SELECTORS = {
 };
 const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4889.0 Safari/537.36";
 
+function checkUrl(url) {
+  // Check if the URL is a valid URL from an Amazon product
+  return url.match(/https:\/\/www.amazon.[a-z]{2,10}\/.*\/dp\/.*/);
+}
+
 // Function for getting the price of a product on Amazon
 async function getPrice(url) {
+  if (!checkUrl(url)) {
+    return { error: "Invalid URL" };
+  }
+
   // Create a new browser
   var browser = await puppeteer.launch({
     headless: true,
