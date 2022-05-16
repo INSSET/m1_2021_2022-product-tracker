ca40a43c1d57c4a5980ea7591fcdaf9c51dfe148
Esteban Restoy <esteban.restoy@etu.uca.fr>
2022-05-16 09:33:39 +0200

    [REFACTOR] pylint refactoring

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
-from scrapper_aliExpress import get_price
 from flask_restx import Resource, Api
+from scrapper import get_price
 
 app = Flask(__name__)
 
@@ -9,19 +9,19 @@ api = Api(app, version='1.0', title='Price Traker for AliExpress API',
 )
 
 @api.route("/getPrice", methods=['GET'])
-class route_get_price(Resource):
-    @api.param('url', 'The url of the product on AliExpress', default="https://fr.aliexpress.com/item/4000262737817.html?spm=a2g0o.productlist.0.0.5c917b48Pc7Rrc&algo_pvid=c341731d-a6ed-41cc-8cac-a187efe74656&algo_exp_id=c341731d-a6ed-41cc-8cac-a187efe74656-11&pdp_ext_f=%7B%22sku_id%22%3A%2212000022064911537%22%7D&pdp_pi=-1%3B29.65%3B-1%3B-1%40salePrice%3BEUR%3Bsearch-mainSearch")
+class RouteGetPrice(Resource):
+    @api.param('url', 'The url of the product on AliExpress',
+        default="https://fr.aliexpress.com/item/4000262737817.html")
     def get(self):
         url = request.args.get('url', default = -1)
         status = 200
-        if(url == -1):
+        if url == -1:
             return "You need to provide an url in named param form url !"
-            
+
         result = get_price(url)
 
-        if(result == -1):
+        if result == -1:
             status = 404
             result = "No product found"
 
         return result, status
-
