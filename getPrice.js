03e49002e6eaf4a461c7f86fd309f0524d8aac84
Théo Cramez <theo.cramez@gmail.com>
2022-05-16 13:40:24 +0200

    Add web server, manage errors

--- getPrice.js
+++ getPrice.js
@@ -7,13 +7,21 @@ const SELECTORS = {
 const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4889.0 Safari/537.36";
 
 function checkUrl(url) {
-  return url.match(/^https:\/\/www.boulanger.com\/ref\/\d+$/);
+  if (url != null && url.length > 0) {
+    return url.match(/^https:\/\/www.boulanger.com\/ref\/\d+$/);
+  }
+  else {
+    return false;
+  }
 }
 
 // Function for getting the price of a product on Boulanger.com
 async function getPrice(url) {
   if (!checkUrl(url)) {
-    return { error: `Invalid URL (${url})` };
+    return {
+      error: `Invalid URL (${url})`,
+      code: 400
+    };
   }
 
   // Create a new browser
@@ -29,27 +37,39 @@ async function getPrice(url) {
   await page.setUserAgent(USER_AGENT);
   try {
     // Go to the url
-    await page.goto(url);
-    // Wait for the page to load
-    await page.waitForSelector(SELECTORS.productTitle, { visible: true });
+    const response = await page.goto(url);
+    // only continue if we don't have an error code
+    if (response.status() !== 200) {
+      return {
+        code: response.status(),
+        error: `Status code ${response.status()}: Product may be unavailable, or website may be down... or crawler was banned.`
+      };
+    }
 
     // get the price of the product
     try {
+      // Wait for the page to load
+      await page.waitForSelector(SELECTORS.productTitle, { visible: true, timeout: 10000 });
+
+      // get product data
       let price = await page.$eval(SELECTORS.productPrice, (el) => el.innerText);
       let prod_name = await page.$eval(SELECTORS.productTitle, (el) => el.innerText);
       return {
+        code: 200,
         price: price,
         prod_name: prod_name,
         seller_name: "Boulanger",
       };
     } catch (error) {
       return {
-        error: `${error}: Product may be unavailable, or user didn't specify a valid URL for a specific product.`,
+        error: `${error}: Website might have changed and is unrecognizable.`,
+        code: 500
       };
     }
   } catch (error) {
     return {
-      error: `${error}: Product may be unavailable, or website may be down... or crawler was banned.`,
+      error: `${error}: Website may be down, or crawler might got banned.`,
+      code: 500
     };
   } finally {
     // Free the resources
--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+import express from 'express';
 import { getPrice } from "./getPrice.js";
 
-var param_url = process.argv[2];
-getPrice(param_url).then((result) => {
-  console.log(result);
+const router = express.Router()
+
+router.get('/getPrice', async (req, res) => {
+  const product_data = await getPrice(req.query.url);
+  res.statusCode = product_data.code;
+  res.send(product_data);
+});
+
+const port = 5000;
+
+const app = express()
+
+app.use(express.json());
+
+app.use(router);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 });
