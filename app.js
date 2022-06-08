2ae8c5fc86f941b946a7ebf58dfce9267a417413
Khechini <khechinimedd@gmail.com>
2022-06-09 01:23:12 +0200

    nodemailer notifier

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+"use strict";
+
 const express = require('express')
 const { ObjectId } = require('mongodb')
 const { connectToDb, getDb } = require('./db')
+const nodemailer = require("nodemailer");
 
 //init app & middleware
 
@@ -22,9 +25,10 @@ connectToDb((err) => {
 
 //routes
 app.get('/', (req, res) => {
+    res.send("Home page")
+})
 
-    console.log("Home page")
-
+app.get('/users', (req, res) => {
     let users = [] 
 
     db.collection('users')
@@ -37,4 +41,45 @@ app.get('/', (req, res) => {
         .catch(() => {
             res.status(500).json({error: 'Could not fetch the documents'})
         })
+})
+
+app.post('/mail/send', (req, res) => {
+    
+    //let testAccount = await nodemailer.createTestAccount();
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+            user: "tproducttrackerccm@gmail.com",
+            pass: "6RsXhHV5L0Kh",
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+        
+    });
+
+    // send mail with defined transport object
+    let info = {
+        from: '"Product Tracker" <producttrackerccm@gmail.com>', // sender address
+        to: "khechinibakr20@gmail.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Notification de prix", // plain text body
+        html: "Output" // html body
+    }
+    
+    transporter.sendMail(info,(err) => {
+        if(err){
+            console.log(err)
+        }else{
+            console.log("Email has sent")
+        }
+    })
+
+    //console.log("Message sent: %s", info.messageId);
+    
+    //main().catch(console.error);
 })
